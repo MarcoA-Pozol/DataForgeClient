e93557664be4e9b265b12fd1701579620eb33b22
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-03-27 09:16:25 -0600

    Updated: Changes in LoginView with a button to manage the authentication action.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import LoginView from './components/Authentication/Login/LoginView.jsx';
+import UsersDataList from './components/DataVisualization/UsersDataList.jsx';
+import RefreshAccessTokenModule from './components/Authentication/JWTManagement/JWTRefresh.jsx';
 
 function App() {
   return (
     <div className="App">
+      <RefreshAccessTokenModule/>
       <LoginView username='JuanMartinez' password='juanmartinez'/>
+      <UsersDataList/>
     </div>
   );
 }
--- src/components/Authentication/Login/LoginView.jsx
+++ src/components/Authentication/Login/LoginView.jsx
@@ -1,30 +1,33 @@
 import React from 'react';
 import axios from 'axios';
 
-const LoginView = async(username, password) => {
-
-    try{
-        // Request for JWT obtention
-        const response = await axios.post('http://localhost:10200/api/token/', {
-            username, // Body
-            password, // Body
-        });
-
-        // Evaluate response
-        if (response.status === 200) {
-            // Save tokens in LocalStorage or a state management library
-            localStorage.setItem('accessToken', response.data.access);
-            localStorage.setItem('refreshToken', response.data.refresh);
-            alert('Valid credentials');
+const LoginView = async({username, password}) => {
+    const handleAuthentication = async() => {
+        try{
+            // Request for JWT obtention
+            const response = await axios.post('http://localhost:10200/api/token/', {
+                username, // Body
+                password, // Body
+            });
+    
+            // Evaluate response
+            if (response.status === 200) {
+                // Save tokens in LocalStorage or a state management library
+                localStorage.setItem('accessToken', response.data.access);
+                localStorage.setItem('refreshToken', response.data.refresh);
+                alert('Valid credentials');
+            }
+        } catch (error) {
+            alert('Invalid credentials');
         }
-    } catch (error) {
-        alert('Invalid credentials');
     }
+    
 
     return (
-        <>
-        
-        </>
+        <div>
+            <h2>This is an authentication page Login</h2>
+            <button onClick={handleAuthentication}>Authenticate</button>
+        </div>
     );
 }
 
