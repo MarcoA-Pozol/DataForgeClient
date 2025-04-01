12b57a7a2cb580d327b230e6e3401a3838ca54bc
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-04-01 09:11:05 -0600

    Updated: Validation layer for register formulary fields.

--- src/components/Authentication/Register/RegisterView.jsx
+++ src/components/Authentication/Register/RegisterView.jsx
@@ -5,6 +5,9 @@ import axios from "axios";
 import "../../../styles/RegisterView.css";
 
 const RegisterView = () => {
+    // Data options 
+    const COUNTRIES = ["USA", "Canada", "UK", "Russia", "China", "Japan", "Mexico", "Colombia", "Peru", "Argentina", "Spain", "Brazil", "Portugal", "Italia"];
+
     // User's formulary data
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("")
@@ -17,10 +20,67 @@ const RegisterView = () => {
 
     const handleRegistering = async (event) => {
         event.preventDefault(); // Prevent page refresh on form submission
+        setErrorMessage(""); // Clear previous errors
+        
+        // Form data validation
+        if (username.length < 3) {
+            setErrorMessage("Username must be at least 3 characters long.");
+            return;
+        } else if (!username.trim()) {
+            setErrorMessage("Username is required.");
+            return;
+        }
 
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailPattern.test(email)) {
+            setErrorMessage("Please enter a valid email address.");
+            return;
+        } else if (!email.trim()) {
+            setErrorMessage("Email is required.");
+            return;
+        }
+
+        if (country.trim() === "") {
+            setErrorMessage("Please select a country.");
+            return;
+        } else if (country.trim in COUNTRIES === false) {
+            setErrorMessage("Please select or insert a valid country.");
+            return;
+        } else if (!country.trim()) {
+            setErrorMessage("Country is required.");
+            return;
+        }
+
+        const urlPattern = /^(https?:\/\/.*\.(?:png|jpg|jpeg|gif|svg))$/i;
+        if (profilePicture && !urlPattern.test(profilePicture)) {
+            setErrorMessage("Profile picture must be a valid image URL (png, jpg, jpeg, gif, svg).");
+            return;
+        }
+
+        const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}$/;;
+        if (!passwordPattern.test(password)) {
+            setErrorMessage("Password must be at least 8 characters long, include a number, a special character, and an uppercase letter.");
+            return;
+        } if (!password.trim()) {
+            setErrorMessage("Password is required.");
+            return;
+        }
+
+        if (password !== repeatPassword) {
+            setErrorMessage("Password and Repeat Password fields must coincide.");
+            return;
+        } else if (!repeatPassword.trim()) {
+            setErrorMessage("Repeated password is required.");
+            return;
+        }
+
+        // Manage requests
         try {
             // Request for user creation
             const response = await axios.post('http://localhost:10200/authentication/register/', {
+                headers: {
+                    "Content-Type": "application/json",
+                },
                 username, 
                 email,
                 country,
@@ -76,16 +136,17 @@ const RegisterView = () => {
                 </label>
 
                 <label>
-                    Profile Picture: <input type="text" value={profilePicture} onChange={(e) => setProfilePicture(e.target.value)} required />
+                    Profile Picture: <input type="text" value={profilePicture} onChange={(e) => setProfilePicture(e.target.value)}/>
+                </label>
+                
+                <label>
+                    Password: <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
                 </label>
 
                 <label>
                     Repeat Password: <input type="password" value={repeatPassword} onChange={(e) => setRepeatPassword(e.target.value)} required />
                 </label>
 
-                <label>
-                    Password: <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-                </label>
 
                 <button type="submit">Register</button>
 
