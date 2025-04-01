24980361e6ffe7166b00032ac214be6e7ead9c6e
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-04-01 08:38:37 -0600

    Added: Register account formulary component design and building.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import LoginView from './components/Authentication/Login/LoginView.jsx';
+import RegisterView from './components/Authentication/Register/RegisterView.jsx';
 import RefreshAccessTokenModule from './components/Authentication/JWTManagement/JWTRefresh.jsx';
 
 function App() {
   return (
     <div className="App">
       <LoginView/>
+      <RegisterView/>
       <RefreshAccessTokenModule/>
     </div>
   );
--- src/components/Authentication/Register/RegisterView.jsx
+++ src/components/Authentication/Register/RegisterView.jsx
@@ -2,59 +2,94 @@
 
 import React, { useState } from "react";
 import axios from "axios";
-import "../../../styles/LoginView.css";
+import "../../../styles/RegisterView.css";
 
-const LoginView = () => {
+const RegisterView = () => {
     // User's formulary data
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("")
+    const [country, setCountry] = useState("");
+    const [profilePicture, setProfilePicture] = useState("")
+    const [password, setPassword] = useState("");
+    const [repeatPassword, setRepeatPassword] = useState("");
+    // Message for invalid formulary data or an error ocurred
+    const [errorMessage, setErrorMessage] = useState("");
 
-    const handleAuthentication = async (event) => {
+    const handleRegistering = async (event) => {
         event.preventDefault(); // Prevent page refresh on form submission
 
         try {
-            // Request for JWT token
-            const response = await axios.post('http://localhost:10200/api/token/', {
-                username, // Body
-                password, // Body
+            // Request for user creation
+            const response = await axios.post('http://localhost:10200/authentication/register/', {
+                username, 
+                email,
+                country,
+                profilePicture,
+                password,
             });
 
-            if (response.status === 200) {
-                // Save tokens in LocalStorage or state management
-                localStorage.setItem('accessToken', response.data.access);
-                localStorage.setItem('refreshToken', response.data.refresh);
-                alert('Valid credentials');
+            // If user is created, obtain a JWT for authentication
+            if (response.status === 201) {
+                // Fetch for a JWT
+                const response_token = await axios.post('https://localhost:10200/api/token/', {
+                    username,
+                    password
+                }); 
+
+                if (response_token.status === 200) {
+                    // Save tokens in LocalStorage or state management
+                    localStorage.setItem('accessToken', response_token.data.access);
+                    localStorage.setItem('refreshToken', response_token.data.refresh);
+                    alert('Valid credentials');
+                } else {
+                    setErrorMessage("Impossible to authenticate the new user.");
+                }
             } else {
-                setErrorMessage('Invalid credentials', response.status)
+                setErrorMessage('It was impossible to create a new user account with the provided data.', response.status)
             }
         } catch (error) {
-            setErrorMessage('An error ocurred during user credentials validation.');
+            setErrorMessage('An error ocurred during account creation.');
         }
     };
 
     return (
-        <div className='login-view'>
+        <div className='register-view'>
             <div className="blobs">
                 <div className="blob"></div>
                 <div className="blob"></div>
                 <div className="blob"></div>
             </div>
 
-            <form className='login-formulary' onSubmit={handleAuthentication}>
-                <h2>Login</h2>
+            <form className='register-formulary' onSubmit={handleRegistering}>
+                <h2>Register</h2>
 
                 <label>
                     Username: <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
                 </label>
 
                 <label>
+                    Email: <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                </label>
+
+                <label>
+                    Country: <input type="text" value={country} onChange={(e) => setCountry(e.target.value)} required />
+                </label>
+
+                <label>
+                    Profile Picture: <input type="text" value={profilePicture} onChange={(e) => setProfilePicture(e.target.value)} required />
+                </label>
+
+                <label>
+                    Repeat Password: <input type="password" value={repeatPassword} onChange={(e) => setRepeatPassword(e.target.value)} required />
+                </label>
+
+                <label>
                     Password: <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
                 </label>
 
-                <button type="submit">Login</button>
+                <button type="submit">Register</button>
 
-                <span id="no-account-button">I do not have an account yet</span>
+                <span id="existing-account-button">I already have an account</span>
             </form>
 
             {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
@@ -62,4 +97,4 @@ const LoginView = () => {
     );
 };
 
-export default LoginView;
+export default RegisterView;
