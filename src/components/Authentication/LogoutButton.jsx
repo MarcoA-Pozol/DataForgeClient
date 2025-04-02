c208a362b4886554ac9314f507d27c092fb45fdb
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-04-02 10:41:23 -0600

    Add: Logout button component with logout process handler.

---
+++ src/components/Authentication/LogoutButton.jsx
@@ -0,0 +1,44 @@
+import React, {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import "../../styles/LogoutButton.css";
+import axios from "axios";
+
+const LogoutButton = () => {
+    const [errorMessage, setErrorMessage] = useState("");
+    const navigate=useNavigate();
+
+    // Get current session refresh token (JWT)
+    const refreshToken = sessionStorage.getItem("refreshToken");
+
+    const handleLogout = async () => {
+        try {
+            const response = await axios.post("localhost:10200/authentication/logout/", {
+                headers: {
+                    Authorization: `bearer ${refreshToken}`,
+                    "Content-Type": "application/json",
+                },
+            });
+
+            if (response.status === 205){
+                // Destroy session token
+                sessionStorage.removeItem("refreshToken");
+                navigate("/login");
+            } else {
+                setErrorMessage(`Impossible to close session.`)
+            }
+        } catch(error) {
+            setErrorMessage(`Impossible to remove refresh token: ${error}`)
+        }
+    }
+
+    return (
+        <>
+            <button className="logout-button" onClick={handleLogout}>
+                Logout
+            </button>
+            {errorMessage && <span>{errorMessage}</span>}
+        </>
+    );
+}
+
+export default LogoutButton;
