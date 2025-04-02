c188bacd7967f82cd5e873e6387a4b9950f93417
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-04-02 17:32:29 -0600

    Fix: Style displaying for AuthenticationView component and importation fixes.

--- src/components/Authentication/AuthenticationView.jsx
+++ src/components/Authentication/AuthenticationView.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import LoginView from "./Login/LoginView.jsx";
 import RegisterView from "./Register/RegisterView.jsx";
 import LogoutButton from "./LogoutButton.jsx";
-import RefreshAccessTokenModule from './components/Authentication/JWTManagement/JWTRefresh.jsx';
+import JWTRefresh from "../Authentication/JWTManagement/JWTRefresh.jsx";
 import "../../styles/AuthenticationView.css";
 
 const AuthenticationView = () => {
 
     return(
         <div className="authentication-view">
-            <RefreshAccessTokenModule/>
+            <JWTRefresh/>
             <LoginView/>
             <RegisterView/>
             <LogoutButton/>
