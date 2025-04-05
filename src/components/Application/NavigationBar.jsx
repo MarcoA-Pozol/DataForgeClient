71cd6cc510b3717b4b2fdad30a19421c687263ae
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-04-04 21:43:48 -0600

    Updated: Styles changes

--- src/components/Application/NavigationBar.jsx
+++ src/components/Application/NavigationBar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import LogoutButton from "../Authentication/LogoutButton.jsx";
+import "../../styles/NavigationBar.css";
 
 const NavigationBar = () => {
 	return (
-		<nav>
+		<nav className="navigation-bar">
 			<Link to="/"><h3>Home</h3></Link>
 			<Link to="/datamanagement"><h3>Data</h3></Link>
 			<LogoutButton/>
