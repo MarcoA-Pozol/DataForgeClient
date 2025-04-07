9fcbfccf96937db0d419e17bb1d2d428072f47c6
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-04-07 08:17:25 -0600

    Updated: Drag and Drop for files in DataFileInputFormulary.

--- src/components/DataVisualization/DataFileInputFormulary.jsx
+++ src/components/DataVisualization/DataFileInputFormulary.jsx
@@ -1,18 +1,30 @@
-import React, {useState} from "react";
+import React, {useState, useCallback} from "react";
+import { useDropzone } from "react-dropzone";
 import "../../styles/DataVisualization/DataFileInputFormulary.css";
 
 const DataFileInputFormulary = () => {
-    const [isFileUploaded, setIsFileUploaded] = useState(false); // State will change once user uploads or drops a file
-    const [uploadedFile, setUploadedFile] = useState(null);
+   const [uploadedFile, setUploadedFile] = useState(null);
 
-    const handleFileUploading = () => {
-        setUploadedFile();
-        if (uploadedFile != null || uploadedFile != null) {
-            setIsFileUploaded(true);
+    // Drop files function
+    const onDrop = useCallback(acceptedFiles => {
+        // Do something with the files
+        const file = acceptedFiles[0];
+        if (file) {
+            setUploadedFile(file);
+            localStorage.setItem("UploadedFileInDataVisualization", file.name);
+            localStorage.setItem("IsFileUploadedInDataVisualization", true);
         }
-        localStorage.setItem("IsFileUploadedInDataVisualization", uploadedFile);
-        localStorage.setItem("UploadedFileInDataVizualization", isFileUploaded);
-    };
+    }, []);
+
+    // Validation of files
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop,
+        accept: {
+            'text/csv': ['.csv'], // Accept .csv files
+            'application/vnd.ms-excel': ['.xls'], // Accept .xls or sheets files
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': ['.xlsx'], // Accept .xlsx files for Excel from office
+        },
+        multiple: false
+    });
 
     const cleanUploadedFile = () => {
         setUploadedFile(null);
@@ -24,10 +36,25 @@ const DataFileInputFormulary = () => {
         <div className="file-upload-container">
             <h3>1. Input The DataFile</h3>
             <p>ℹ️ Upload a file in csv, xlsx or xls format. You can drag and drop down below.</p>
-            <form onChange={handleFileUploading} className="file-upload-formulary">
+
+            {/* Drop fIles here */}
+            <div {...getRootProps({ className: 'dropzone' })}>
+                <input {...getInputProps()} />
+                {uploadedFile ? (
+                    <p id="file-info">✅ File selected: <strong>{uploadedFile.name}</strong></p>
+                ) : (
+                    <p>🗂️ Drag and drop a file here, or click to select one</p>
+                )}
+            </div>
+
+            {/* Browse files manually */}
+            {/*<form onChange={(e) => onDrop(e.target.files)} accept=".csv, .xlsx, .xls" className="file-upload-formulary">
                 <label>File (csv, xlsx, xls) <input type="file" value={uploadedFile}></input></label>
-            </form>
-            <button id="clean-uploaded-file-button" onClick={cleanUploadedFile}>Clean</button>
+            </form>*/}
+            
+            {/* Clean selected file */}
+            {uploadedFile ? (<button className="clean-uploaded-file-button" onClick={cleanUploadedFile}>Clean</button>)
+            : (<button className="clean-uploaded-file-button hided" onClick={cleanUploadedFile}>Clean</button>)}
         </div>
     );
 }
