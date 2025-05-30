6ef2e2362a4a29c208deddd01b5cf15462d55bbf
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-05-30 10:07:22 -0600

    Removed unecesary components for simplicity

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomeView from "../components/Application/HomeView";
-import DataManagementView from "../components/Application/DataManagementView";
 import AuthenticationView from "../components/Authentication/AuthenticationView";
 import PageNotFoundView from "../components/PageNotFoundView";
 import DataVisualizationView from "../components/DataVisualization/DataVisualizationView";
@@ -13,8 +12,6 @@ const AppRoutes = () => {
                 <Routes>
                     <Route path="/" element={<HomeView/>}/>
                     <Route path="/datavisualization" element={<DataVisualizationView/>}/>
-                    <Route path="/dataimportexport" element={<DataManagementView/>}/>
-                    <Route path="/datareporting" element={<DataManagementView/>}/>
                     <Route path="/authentication" element={<AuthenticationView/>}/>
                     <Route path="*" element={<PageNotFoundView/>} />
                 </Routes>
