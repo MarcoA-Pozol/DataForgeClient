48d978505affd53b17b753d9e7be43ec331521c8
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-05-30 20:28:24 -0600

    Fixed bars were not visible on data load, nor labels on top and bottom.

--- src/components/DataVisualization/ChartContainer.tsx
+++ src/components/DataVisualization/ChartContainer.tsx
@@ -11,14 +11,18 @@ const DataChartContainer = ({data, xKey, yKey}:DataChartContainerProps) => {
   const width:number = window.innerWidth;
   const height:number = window.innerHeight;
 
+  const actualXKey = "item";
+  console.log(data, xKey, actualXKey);
+  
+
   return(
     <BarChart className="chart-block" width={width*.6} height={height*.7} style={{marginLeft:20}} data={data}>
       <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey={xKey} angle={-45} textAnchor="end" height={60} style={{fontSize:12}}/>
-      <YAxis dataKey={yKey}/>
+      <XAxis dataKey={actualXKey} angle={-45} textAnchor="end" height={60} tick={{ fontSize: 12 }}/>
+      <YAxis/>
       <Tooltip />
       <Legend />
-      <Bar dataKey="total" name={xKey} fill="#FFFFFF"/>
+      <Bar dataKey={data[0]?.[yKey] !== undefined ? yKey : "total"} name={xKey} fill="#A2CADC" label={({ x, y, width, value }) => (<text x={x! + width! / 2} y={y! - 5} fill="#FFFFFF" textAnchor="middle" fontSize={14}>{value}</text>)}/>
     </BarChart>
   );
 }
